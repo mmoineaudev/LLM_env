196cc6cf9415072a557281a0a9d1a5a968a63786
Neo <neo@example.com>
2026-05-26 20:53:08 +0200

    oopsie

--- llama.cpp.ubuntu24.sh
+++ llama.cpp.ubuntu24.sh
@@ -169,8 +169,8 @@ step_cuda_toolkit() {
 
             wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2404/arm64/cuda-ubuntu2404.pin
             sudo mv cuda-ubuntu2404.pin /etc/apt/preferences.d/cuda-repository-pin-600
-            wget https://developer.download.nvidia.com/compute/cuda/13.2.1/local_installers/cuda-repo-ubuntu2404-13-2-local_13.2.1-595.58.03-1_amd64.deb
-            sudo dpkg -i cuda-repo-ubuntu2404-13-2-local_13.2.1-595.58.03-1_amd64.deb
+            wget https://developer.download.nvidia.com/compute/cuda/13.2.1/local_installers/cuda-repo-debian12-13-2-local_13.2.1-595.58.03-1_arm64.deb
+            sudo dpkg -i cuda-repo-ubuntu2404-13-2-local_13.2.1-595.58.03-1_arm64.deb
             sudo cp /var/cuda-repo-ubuntu2404-13-2-local/cuda-*-keyring.gpg /usr/share/keyrings/
             sudo apt-get update
             sudo apt-get -y install cuda-toolkit-13-2
